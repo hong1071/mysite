d4557959f347a5f54db682c1328bfd4d0f185188
changmok <hong1071@nate.com>
2021-10-12 13:50:34 +0900

    action & actionFactory

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -15,17 +15,21 @@ public class UserController extends HttpServlet {
 	
 		request.setCharacterEncoding("utf-8");
 		
-		String action = request.getParameter("a");
-		if("joinform".equals(action)) {
-			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
-		}
-		else if("join".equals(action)) {
-			
-		} 
-		else {
-			MvcUtil.redirect("/mysite02", request, response);
-		}
-
+		String actionName = request.getParameter("a");
+//		if("joinform".equals(action)) {
+//			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
+//		}
+//		else if("join".equals(action)) {
+//			
+//		} 
+//		else {
+//			MvcUtil.redirect("/mysite02", request, response);
+//		}
+		
+		ActionFactory af = new UserActionFactory();
+		Action action = af.getAction(actionName);
+		action.execute();
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
