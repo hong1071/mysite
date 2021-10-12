365f9033d41a95db224536427518a1bc7e6b996f
changmok <hong1071@nate.com>
2021-10-12 12:19:23 +0900

    user service

--- mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite02/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,26 +1,35 @@
 package com.douzone.mysite.controller;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.douzone.web.util.MvcUtil;
 
-public class mainController extends HttpServlet {
+public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
 		request.setCharacterEncoding("utf-8");
-		System.out.println("Main Controller Called");
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main/index.jsp");
-		rd.forward(request, response);
+		
+		String action = request.getParameter("a");
+		if("joinform".equals(action)) {
+			MvcUtil.forward("/WEB-INF/views/user/joinform.jsp", request, response);
+		}
+		else if("join".equals(action)) {
+			
+		} 
+		else {
+			MvcUtil.redirect("/mysite02", request, response);
+		}
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		doGet(request, response);
 	}
 
