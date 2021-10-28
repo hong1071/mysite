43ee2c8163140a48bcd9b9fd25cf5a5362af7f8f
changmok <hong1071@nate.com>
2021-10-28 12:20:06 +0900

    AppConfig

---
+++ mysite04/src/main/java/com/douzone/config/app/DBConfig.java
@@ -0,0 +1,25 @@
+package com.douzone.config.app;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DBConfig {
+	
+	@Bean
+	public DataSource dataSource() {
+		
+		BasicDataSource dataSource = new BasicDataSource();
+		dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8");
+		dataSource.setUsername("webdb");
+		dataSource.setPassword("webdb");
+		dataSource.setInitialSize(100);			//connection pool의 최대 갯수
+		dataSource.setMaxActive(200);
+		
+		return dataSource;
+	}
+}
---
+++ mysite04/src/main/java/com/douzone/mysite/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.douzone.mysite.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Import;
+
+import com.douzone.config.app.DBConfig;
+
+/*
+ * 
+ * 모으는 역할
+ * 
+ * */
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan({"com.douzone.mysite.service", "com.douzone.mysite.repository", "com.douzone.mysite.aspect"})
+@Import({DBConfig.class})
+public class AppConfig {
+
+}
