bfb400d8e692487e06db8d1408c01a3cdd536dda
changmok <hong1071@nate.com>
2021-11-01 17:32:51 +0900

    mysite06, SpringBoot로 변환

--- mysite05/src/main/java/com/douzone/mysite/repository/SiteRepository.java
+++ mysite05/src/main/java/com/douzone/mysite/repository/SiteRepository.java
@@ -1,5 +1,7 @@
 package com.douzone.mysite.repository;
 
+import javax.sql.DataSource;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,6 +10,10 @@ import com.douzone.mysite.vo.SiteVo;
 
 @Repository
 public class SiteRepository {
+	
+	@Autowired
+	private DataSource dataSource;
+	
 	@Autowired
 	private SqlSession sqlSession;
 
